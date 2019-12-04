e00c294aca5687576c7aed7bc90ce2575f889fb4
Ryan Gonzales <helloryantg@gmail.com>
2019-12-04 09:32:23 -0800

    Removes create-react-app uneeded code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+
+
+const AppContainer = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: black;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <AppContainer className="App">
+      App
+    </AppContainer>
+  )
 }
 
-export default App;
+export default App
